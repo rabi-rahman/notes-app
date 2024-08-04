e10459f2138414ab7be3d4ec203dcdf18e0ba01b
rabeeh_rahman <rabeehrahman.rr@gmail.com>
2024-08-04 21:05:54 +0530

    created Add and edit Note card

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import NoteCard from '../components/NoteCard'
 import { MdAdd } from 'react-icons/md'
+import AddEditNotes from '../components/AddEditNotes'
+import Modal from 'react-modal'
 
 const Home = () => {
+
+  const [editNote,setEditNote] = useState({
+    isShown:false,
+    type:"add",
+    data:null,
+  });
+
   return (
     <>
     <Navbar />
@@ -21,9 +30,26 @@ const Home = () => {
       </div>
     </div>
 
-    <button className='w-12 h-12 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10' onClick={() => {}}>
+    <button className='w-12 h-12 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10'
+    onClick={() => {
+      setEditNote({ isShown:true, type:'add', data:null })
+    }}>
       <MdAdd className='text=[32px] text-white' />
     </button>
+
+    <Modal
+    isOpen={editNote.isShown}
+    onRequestClose={() => {}}
+    style={{
+      overlay:{
+        backgroundColor: "rgba(0,0,0,0.2)",
+      },
+    }}
+    contentLabel=''
+    className='w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll'>
+      <AddEditNotes />   
+    </Modal>
+
     </>
   )
 }
